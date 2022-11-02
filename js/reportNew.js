481509734477033706de0d7781179252952fda35
xiaokai <1020849724@qq.com>
2022-11-02 15:10:51 +0800

    修改报告页点赞 小凯

--- js/reportNew.js
+++ js/reportNew.js
@@ -1,3 +1,11 @@
+// 获取全部广告图片
+var pics = document.getElementsByClassName("pic");
+for (let item of pics) {
+  item.addEventListener("click", function () {
+    window.location.href = "../shop/shopProduct.html";
+  });
+}
+
 // 体检报告最新部分点赞
 var zans_1 = null;
 function zan_1() {
@@ -33,23 +41,5 @@ for (let i = 0; i < zans_1.length; i++) {
       this.style.backgroundSize = "12px";
     }
   });
-    zans_1 = document.getElementsByClassName('zan');
-}
-zan_1();
-for (let i = 0; i < zans_1.length; i++) {
-    zans_1[i].setAttribute("index", 0);
-    zans_1[i].addEventListener("click", function () {
-        this.setAttribute("index", this.getAttribute("index") - 0 + 1);
-        if (this.getAttribute("index") % 2) {
-            this.style.backgroundImage = "Url(../img/icon/xinRedh.png)";
-            this.style.backgroundSize = "12px";
-            this.innerHTML = this.innerHTML - 0 + 1;
-        } else {
-            this.style.backgroundImage = "Url(../img/icon/use5.png)";
-            this.style.backgroundSize = "12px";
-            this.innerHTML = this.innerHTML -= 1;
-        }
-
-    })
-
+  zans_1 = document.getElementsByClassName("zan");
 }
